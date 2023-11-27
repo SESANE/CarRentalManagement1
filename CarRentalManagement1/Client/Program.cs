46cbd4839a233d86ce28360a08b65003c09ec680
sesan <sesan@MSI>
2023-11-28 00:58:00 +0800

    Add project files.

---
+++ CarRentalManagement1/Client/Program.cs
@@ -0,0 +1,18 @@
+using CarRentalManagement1.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("CarRentalManagement1.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("CarRentalManagement1.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+await builder.Build().RunAsync();
---
+++ CarRentalManagement1/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CarRentalManagement1.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
